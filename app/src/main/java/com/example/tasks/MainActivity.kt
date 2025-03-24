913759de9d3768e24fdcf33f7be0c380b38f724b
Josu <vjosuelc@gmail.com>
2025-03-23 21:52:28 -0600

    Implementacion de login

--- app/src/main/java/com/example/tasks/MainActivity.kt
+++ app/src/main/java/com/example/tasks/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
